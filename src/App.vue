a8bd19b38a63dec212e92d176aaa32ad8704b0e5
ReljaMih <reljamihajlovic@outlook.com>
2023-10-27 23:03:04 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    Nav
+    <section class="xl:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+    <section class="padding">DDA</section>
+    <section class="padding"></section>
+    <section class="padding"></section>
+    <section class="padding"></section>
+    <section class="padding"></section>
+    <section class="padding"></section>
+    <section class="padding"></section>
+    <section></section>
+    <section></section>
+    <section></section>
+  </main>
+
+</template>
+
+<style scoped>
+</style>
